46d243841eeffc292c8ebf13ea03badaabce7dbe
LeagLi23 <Lisbethesperanzaa@gmail.com>
2023-07-10 22:25:55 -0400

    actualizacion

--- clase1.py
+++ clase1.py
@@ -1,23 +1,21 @@
-
 f = open("elprofeejevi.txt", "r") #TODO: Ususalmente se deja en "r" para no modificar la base de datos
 #next(f)
 sum = 0
-is_float_found = False
-
 def is_float(string):
-    string = string.replace("\n", "")
+    string.replace("\n","")
     if string.replace(".", "").isnumeric():
         return True
     else:
         return False
 
-with open("elprofeejevi.txt", "r") as f:
-    for line in f:
-        if is_float(line):
-            sum += float(line)
-            is_float_found = True
+for line in f:
+    if (is_float(line)):
+        sum = sum + float(line)
+    else:
+        print("bue miloco")
 
-if is_float_found:
-    print(sum)
-else:
-    print("No se encontraron números válidos en el archivo.")
+#avr = sum/198
+print(sum)
+print(is_float("klk"))
+print(is_float("125584.8380\n"))
+print(type(f))
