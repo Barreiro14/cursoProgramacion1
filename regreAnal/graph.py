a46adb9d9aa2a7d80ecccd8059a660c19fc09222
Barry <g.barreiro140@gmail.com>
2023-08-26 12:01:38 -0400

    add regreanal

--- regreAnal/graph.py
+++ regreAnal/graph.py
@@ -1,1 +1,25 @@
-f
+import numpy as np
+from values import Values
+from syssolver import Solver
+import matplotlib.pyplot as plt
+
+def Graph(filename, n, xlabel = '', ylabel = ''):
+    t = np.arange(Values(filename)[0][0], Values(filename)[0][-1], 0.2)
+    p = 0
+    i = 0
+    while i <= n: p = p + Solver(filename, n)[i]*(t**i); i = i + 1
+    plt.plot(Values(filename)[0], Values(filename)[1], 'bo', label="Data")
+    if n == 1:
+        plt.plot(t, p, 'k', label="First degree polynomial")
+    elif n == 2:
+        plt.plot(t, p, 'k', label="Second degree polynomial")
+    elif n == 3:
+        plt.plot(t, p, 'k', label="Third degree polynomial")
+    else:
+        plt.plot(t, p, 'k', label="{}th degree polynomial".format(n))
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    #plt.legend()
+    #plt.ylim([0, 0.015])
+    plt.show()
+    #plt.savefig('{}th degree'.format(n))
--- regreAnal/main.py
+++ regreAnal/main.py
@@ -1,1 +1,25 @@
-f
+import argparse
+from pathlib import Path
+from graph import Graph
+
+
+def main():
+	parser = argparse.ArgumentParser(description='Description of your program')
+	parser.add_argument('-f','--path', help='Path for the data file', required=True)
+	parser.add_argument('-n','--degree', help='Degree of the regression', required=True)
+	parser.add_argument('-x','--xlabel', help='X axis label', required=False)
+	parser.add_argument('-y','--ylabel', help='Y axis label', required=False)
+	args = vars(parser.parse_args())
+
+	file = Path(args['path'])
+	if file.is_file():
+		if args['xlabel'] is not None and args['ylabel'] is not None:
+			Graph(args['path'], int(args['degree']), args['xlabel'], args['ylabel'])
+		else:
+			Graph(args['path'], int(args['degree']))
+	else:
+		print("Introduce un archivo valido miloco")
+	#main function
+
+if __name__ == '__main__':
+	main()
